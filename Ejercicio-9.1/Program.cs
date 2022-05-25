dc2de0b46b89eac285222599a67514cb1cc56983
martinagustin <g4rciamartin@gmail.com>
2022-05-25 19:49:58 -0300

    creamos bucles para recorrer

--- Ejercicio-9.1/Program.cs
+++ Ejercicio-9.1/Program.cs
@@ -1,3 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
-//varables
-Console.WriteLine("Hello, World!");
+//variables
+int size=0;
+Console.WriteLine("Indicar longitud del vector");
+size = int.Parse(Console.ReadLine());
+int[] vectorDirecto=new int[size];
+int[] vectorOpuesto = new int[size];
+//funcion Random
+Random aleatorio=new Random();
+//bucle de carga de vector
+for(int i = 0; i < size; i++)
+{
+    vectorDirecto[i] =aleatorio.Next(1,101);
+}
+//bucle para invertir vector
+Console.WriteLine("-----------");
+//bucle de mostrar elementos del vector
+Console.WriteLine("Elementos dentro del vector");
+foreach(int i in vectorDirecto)
+{
+    Console.Write($"{i} ");
+}
